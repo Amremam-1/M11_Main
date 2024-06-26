1913ff3f7472e02f28c3933a67b5bb33ea0edaba
Amr Imam <emama2337@gmail.com>
2024-06-26 20:39:59 +0300

    ddd

--- src/components/utils/Header/Header.jsx
+++ src/components/utils/Header/Header.jsx
@@ -98,7 +98,7 @@ const Header = () => {
                     className={`${styles.navLink} ${
                       activeLink === item.link ? styles.active : ""
                     }`}
-                    href={item.link}
+                    to={item.link}
                     onClick={() => toggle(item.link)}
                   >
                     {item.titleAr}
